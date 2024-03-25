99382cb36d60678fdb08085477de0422fdcebc9d
Bert0ns <121551954+Bert0ns@users.noreply.github.com>
2024-03-25 01:00:40 +0100

    Implemented Fraction class

---
+++ src/main/java/org/com/ConicalCurves.java
@@ -0,0 +1,14 @@
+package org.com;
+
+public class ConicalCurves {
+    public static void main(String[] args) {
+        Fraction f1 = new Fraction(12, 4);
+        Fraction f2 = new Fraction(15);
+        Fraction f3 = new Fraction( 1.5);
+        Fraction f4 = new Fraction(-4, 18);
+        Fraction f5 = new Fraction(8, -150);
+
+
+        System.out.println(f1.toString() + f2 + f3 + f4 + f5);
+    }
+}
---
+++ src/main/java/org/com/Fraction.java
@@ -0,0 +1,156 @@
+package org.com;
+
+import java.util.Objects;
+
+public class Fraction extends Number{
+    private int numerator;
+    private int denominator;
+
+    /**
+     * Creates an object Fraction, in minimal form
+     * @param numerator numerator of the fraction
+     * @param denominator denominator of the fraction
+     */
+    public Fraction(int numerator, int denominator) {
+        if(denominator == 0) {
+            throw new IllegalArgumentException("Denominator is zero");
+        }
+
+        this.numerator = numerator;
+        this.denominator = denominator;
+
+        ConvertToMinimalForm();
+    }
+
+    /**
+     * Creates a Fraction Object with denominator = 1, in minimal form
+     * @param numerator numerator of the fraction
+     */
+    public Fraction(int numerator) {
+        this.numerator = numerator;
+        this.denominator = 1;
+
+        ConvertToMinimalForm();
+    }
+
+    public Fraction(double number)
+    {
+        denominator = 1;
+
+        do{
+            denominator = denominator * 10;
+        }while((number * denominator) % 10 != 0);
+
+        denominator=denominator / 10;
+        this.numerator = (int)(number*denominator);
+
+        ConvertToMinimalForm();
+    }
+
+    private void ConvertToMinimalForm()
+    {
+        if(denominator < 0)
+        {
+            numerator *= -1;
+            denominator *= -1;
+        }
+
+        int mcd = MCD(Math.abs(numerator), Math.abs(denominator));
+        numerator /= mcd;
+        denominator /= mcd;
+    }
+
+
+    private int MCD(int a, int b) {
+        int t;
+        while (b != 0) {
+            t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    /**
+     *
+     * @return Fraction's numerator
+     */
+    public int getNumerator() {
+        return numerator;
+    }
+
+    /**
+     *
+     * @return Fraction's denominator
+     */
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public Fraction addFraction(Fraction f)
+    {
+        denominator *= f.getDenominator();
+        numerator = numerator * f.getDenominator() + f.getNumerator()*denominator;
+        ConvertToMinimalForm();
+        return this;
+    }
+    public Fraction subtractFraction(Fraction f)
+    {
+        denominator *= f.getDenominator();
+        numerator = numerator * f.getDenominator() - f.getNumerator()*denominator;
+        ConvertToMinimalForm();
+        return this;
+    }
+    public Fraction multiply(Fraction f)
+    {
+        numerator *= f.getNumerator();
+        denominator *= f.getDenominator();
+        ConvertToMinimalForm();
+        return this;
+    }
+    public Fraction divide(Fraction f)
+    {
+        numerator *= f.getDenominator();
+        denominator *= f.getNumerator();
+        ConvertToMinimalForm();
+        return this;
+    }
+
+    @Override
+    public int intValue() {
+        return numerator/denominator;
+    }
+
+    @Override
+    public long longValue() {
+        return (long)numerator/denominator;
+    }
+
+    @Override
+    public float floatValue() {
+        return  numerator/(float)denominator;
+    }
+
+    @Override
+    public double doubleValue() {
+        return numerator/(double)denominator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fraction fraction = (Fraction) o;
+        return getNumerator() == fraction.getNumerator() && getDenominator() == fraction.getDenominator();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumerator(), getDenominator());
+    }
+
+    @Override
+    public String toString() {
+        return "("+numerator + "/" + denominator+")";
+    }
+}
